a85e00388522e32a248f43cb09c0c4568c5831d1
Deepak Rakshit <deepakrakshit505@gmail.com>
2026-03-29 22:34:25 +0530

    chore(services): update package initialization

--- services/__init__.py
+++ services/__init__.py
@@ -1,6 +1,5 @@
-from services.news_service import NewsService
 from services.network_service import NetworkService
 from services.search_service import SearchService
 from services.weather_service import WeatherService
 
-__all__ = ["WeatherService", "NewsService", "NetworkService", "SearchService"]
+__all__ = ["WeatherService", "NetworkService", "SearchService"]
